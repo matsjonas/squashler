f26e008e9276c2a6803e748df55c1b92b18366bb
Jonas Andersson <me@matsjonas.com>
2013-08-19 22:54:54 +0200

    First draft of a RESTful api.

---
+++ app/controllers/RestfulAPI.java
@@ -0,0 +1,44 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class RestfulAPI extends Controller {
+
+    public static Result player(long id) {
+        return play.mvc.Results.TODO;
+    }
+
+    public static Result createPlayer() {
+        return play.mvc.Results.TODO;
+    }
+
+    public static Result updatePlayer(long id) {
+        return play.mvc.Results.TODO;
+    }
+
+    public static Result deletePlayer(long id) {
+        return play.mvc.Results.TODO;
+    }
+
+    public static Result game(long id) {
+        return play.mvc.Results.TODO;
+    }
+
+    public static Result createGame() {
+        return play.mvc.Results.TODO;
+    }
+
+    public static Result updateGame(long id) {
+        return play.mvc.Results.TODO;
+    }
+
+    public static Result deleteGame(long id) {
+        return play.mvc.Results.TODO;
+    }
+
+    public static Result games() {
+        return play.mvc.Results.TODO;
+    }
+
+}
