6309045e2bc95c5692bf7f39d01e931508ca1846
Jonas Andersson <me@matsjonas.com>
2013-08-20 19:43:25 +0200

    Implement RestfulAPI.player(long) and RestfulAPI.games().

--- app/controllers/RestfulAPI.java
+++ app/controllers/RestfulAPI.java
@@ -1,12 +1,21 @@
 package controllers;
 
+import models.Game;
+import models.Player;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.codehaus.jackson.node.ArrayNode;
+import org.codehaus.jackson.node.ObjectNode;
 import play.mvc.Controller;
 import play.mvc.Result;
+import util.JsonUtils;
+
+import java.util.List;
 
 public class RestfulAPI extends Controller {
 
     public static Result player(long id) {
-        return play.mvc.Results.TODO;
+        Player player = Player.byId(id);
+        return ok(getPlayerJsonNode(player));
     }
 
     public static Result createPlayer() {
@@ -38,7 +47,31 @@ public class RestfulAPI extends Controller {
     }
 
     public static Result games() {
-        return play.mvc.Results.TODO;
+        List<Game> games = Game.all();
+        ArrayNode result = JsonUtils.newArrayNode();
+        for (Game game : games) {
+            result.add(getGameJsonNode(game));
+        }
+        return ok(result);
+    }
+
+    private static ObjectNode getPlayerJsonNode(Player player) {
+        ObjectNode result = JsonUtils.newObjectNode();
+        result.put("id", player.id);
+        result.put("name", player.name);
+        return result;
+    }
+
+    private static ObjectNode getGameJsonNode(Game game) {
+        ObjectNode node = JsonUtils.newObjectNode();
+        node.put("id", game.id);
+        node.put("date", DateFormatUtils.format(game.date, "yyyy-MM-dd HH:mm"));
+        node.put("gameNbr", game.gameNbr);
+        node.put("playerLeft", getPlayerJsonNode(game.playerLeft));
+        node.put("playerRight", getPlayerJsonNode(game.playerRight));
+        node.put("pointsLeft", game.pointsLeft);
+        node.put("pointsRight", game.pointsRight);
+        return node;
     }
 
 }
