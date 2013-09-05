57e2e90d4fd7439226114c2bfd80e2cc6665b17a
Jonas Andersson <me@matsjonas.com>
2013-09-05 21:56:36 +0200

    Extract a method for updating game information in Game.java.

--- app/models/Game.java
+++ app/models/Game.java
@@ -64,6 +64,11 @@ public class Game extends Model {
 
     public static Game insert(Date date, Player playerLeft, Player playerRight, int pointsLeft, int pointsRight) {
         Game game = new Game();
+        update(game, date, playerLeft, playerRight, pointsRight, pointsLeft);
+        return game;
+    }
+
+    public static void update(Game game, Date date, Player playerLeft, Player playerRight, int pointsRight, int pointsLeft) {
         game.date = date;
         game.gameNbr = getMaxGameNbr(date) + 1;
         game.playerLeft = playerLeft;
@@ -73,7 +78,6 @@ public class Game extends Model {
 
         game.save();
         recalculateGameNbrsFrom(game);
-        return game;
     }
 
     private static void recalculateGameNbrsFrom(Game game) {
