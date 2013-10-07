a451299566484325734d47a7b13de3838fd804b4
Jonas Andersson <me@matsjonas.com>
2013-10-07 22:45:07 +0200

    Make it possible to add GameGroups.

--- app/controllers/GameGroupPortal.java
+++ app/controllers/GameGroupPortal.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import models.GameGroup;
+import org.apache.commons.lang3.StringUtils;
+import play.data.DynamicForm;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.gameGroups;
@@ -7,6 +10,25 @@ import views.html.gameGroups;
 public class GameGroupPortal extends Controller {
 
     public static Result gameGroups() {
-        return ok(gameGroups.render(models.GameGroup.all()));
+        return ok(gameGroups.render(GameGroup.all(), DynamicForm.form()));
     }
+
+    public static Result insertGameGroup() {
+        DynamicForm playerForm = DynamicForm.form().bindFromRequest();
+
+        String name = playerForm.data().get("name");
+        if (StringUtils.isBlank(name)) {
+            playerForm.reject("name", "Name must not be empty.");
+        }
+
+        if (playerForm.hasErrors()) {
+            System.out.println("GameGroupPortal.insertGameGroup 1");
+            return badRequest(gameGroups.render(GameGroup.all(), playerForm));
+        } else {
+            GameGroup.insert(name);
+            System.out.println("GameGroupPortal.insertGameGroup 2");
+            return redirect(routes.GameGroupPortal.gameGroups());
+        }
+    }
+
 }
